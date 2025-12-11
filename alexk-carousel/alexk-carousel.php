703c5a4ec622804723076903ac30946eb18f5871
Drew Dudak <ddudak@gmail.com>
2025-12-11 16:10:55 -0500

    added shortcode

--- alexk-carousel/alexk-carousel.php
+++ alexk-carousel/alexk-carousel.php
@@ -22,3 +22,21 @@ function alexk_carousel_dev_notice() {
     echo '<p style="opacity:0.6; text-align:center; margin-top:1rem;">Alex K Image Carousel plugin is active (dev mode).</p>';
 }
 add_action('admin_footer', 'alexk_carousel_dev_notice');
+
+/**
+ * Shortcode: [alexk_carousel]
+ * For now: just proves the plugin can render on the front-end.
+ */
+function alexk_carousel_shortcode($atts = []) {
+    // Later we'll accept options like count, category, etc.
+    $atts = shortcode_atts([
+        'message' => 'Alex K Carousel shortcode is working.',
+    ], $atts, 'alexk_carousel');
+
+    // Always return a string from shortcodes (don’t echo).
+    $safe_message = esc_html($atts['message']);
+
+    return '<div class="alexk-carousel">' . $safe_message . '</div>';
+}
+
+add_shortcode('alexk_carousel', 'alexk_carousel_shortcode');
