5ecf3567408cd725f6eef4a611032bb5bfcdd959
Drew Dudak <ddudak@gmail.com>
2025-12-11 15:33:04 -0500

    initial plugin commit

---
+++ client-carousel/client-carousel.php
@@ -0,0 +1,23 @@
+<?php
+// File: wp-content/plugins/client-carousel/client-carousel.php
+
+/**
+ * Plugin Name: Client Carousel
+ * Description: Simple random image carousel for a client site.
+ * Version: 0.1.0
+ * Author: Drew Dudak
+ */
+
+if (!defined('ABSPATH')) {
+    // Abort if this file is loaded directly.
+    exit;
+}
+
+/**
+ * Temporary test hook so we can confirm the plugin is active.
+ * We will remove this once real functionality is in place.
+ */
+function client_carousel_dev_notice() {
+    echo '<p style="opacity:0.6; text-align:center; margin-top:1rem;">Client Carousel plugin is active (dev mode).</p>';
+}
+add_action('admin_footer', 'client_carousel_dev_notice');
