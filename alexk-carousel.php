533b9a9a3cb43991870cd006f773bd10c5ab2b95
Drew Dudak <ddudak@gmail.com>
2025-12-17 21:26:16 -0500

    renaming elements so they make more sense

--- alexk-carousel.php
+++ alexk-carousel.php
@@ -116,8 +116,8 @@ function alexk_add_carousel_checkbox_field($form_fields, $post) {
 
         // ),
         'html' => sprintf(
-            '<div class="alexk-carousel-admin-field">
-                <label class="alexk-carousel-admin-label">
+            '<div class="alexk-carousel-rightside-container">
+                <label class="alexk-carousel-rightside-label">
                     <input type="checkbox"
                         name="attachments[%d][alexk_include_in_carousel]"
                         value="1"
@@ -125,8 +125,8 @@ function alexk_add_carousel_checkbox_field($form_fields, $post) {
                     />
                     Include in carousel
                 </label>
-                <div class="alexk-carousel-admin-help">
-                    When checked, this image will appear in the Alex K carousel when using the <code>[alexk_carousel]</code> shortcode without IDs. 
+                <div class="alexk-carousel-checkbox-details">
+                    When checked, this image will appear in the <code>Alex K image carousel</code>
                 </div>
             </div>',
             (int) $post-> ID,
